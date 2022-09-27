dac66717b7158a1328b8986235038033e88a5f74
Thybris Vandenbroucke <r0799842@student.vives.be>
2022-09-27 14:35:55 +0200

    code

---
+++ demoLes2h/demoLes2/src/components/Captcha.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const captchaElements = ref([
+    {
+        image: 'https://image.shutterstock.com/image-photo/skeptic-surprised-cat-thinking-dont-260nw-1905929728.jpg',
+        isSelected: false,
+        isfunny: true,
+    },
+    {
+        image:'https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg',
+        isSelected: false,
+        isfunny: true,
+    },
+    {
+        image:'https://media.s-bol.com/grOq0M7nypG/550x827.jpg',
+        isSelected: false,
+        isfunny: true,
+    },
+    {
+        image:'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+        isSelected:false,
+        isfunny: false,
+    }
+]);
+
+const confirm = function() {
+    console.log("User clicked the confirm button");
+
+    const isCorrect = captchaElements.value.reduce(
+        (result, captchaObj) =>  result && (captchaObj.isfunny == captchaObj.isSelected), true
+    )
+
+     console.log(`IsCorrect? ${isCorrect}`);
+}
+
+</script>
+
+<template>
+    <div>
+        <img class="image" 
+                v-for="(item, i) in captchaElements" 
+                            :src="item.image" 
+                            :key="`cpt${i}`"
+                            :class="{selected: item.isSelected}"
+                            @click="item.isSelected =! item.isSelected"
+        />
+
+        <button @click="confirm">Verify Captcha</button>
+    </div>
+    
+    
+</template>
+
+<style scoped>
+    .image {
+        width: 100px;
+        height: 100px;
+        margin: 10px;
+        border: solid grey 4px;
+        object-fit: contain;
+    }
+
+    .selected {
+        border: solid lime 4px;
+    }
+
+</style>
---
+++ demoLes2h/demoLes2/src/views/CaptchaView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Captcha from '@/components/Captcha.vue';
+
+</script>
+
+<template>
+    <div>
+      <h1>captcha views</h1>
+      <Captcha />
+
+    </div>
+  </template>
